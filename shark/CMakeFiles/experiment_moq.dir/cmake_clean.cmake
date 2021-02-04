0554a8a8c65cee40d2a3cbb7b3fb331c0fbc1154
Anguilla anguilla <pocs.anguilla@gmail.com>
2021-02-04 19:05:08 +0100

    Add Shark experiment data

---
+++ shark/CMakeFiles/experiment_moq.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/experiment_moq.dir/src/moq/benchmarks.cpp.o"
+  "CMakeFiles/experiment_moq.dir/src/moq/experiments.cpp.o"
+  "experiment_moq"
+  "experiment_moq.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/experiment_moq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ shark/CMakeFiles/fitness.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fitness.dir/src/fitness.cpp.o"
+  "fitness"
+  "fitness.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fitness.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
